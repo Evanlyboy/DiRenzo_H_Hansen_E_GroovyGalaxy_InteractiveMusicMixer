0b4de99305f9a75497500546cb0394095245f991
Evan <39568306+Evanlyboy@users.noreply.github.com>
2022-03-12 20:22:19 -0500

    added some stellar graphics part hannah part me

--- js/main.js
+++ js/main.js
@@ -17,19 +17,22 @@
 
 // Drag and Drop Variables
 const draggableButtons = document.querySelectorAll(".buttonGroup *"),
-      dropZones = document.querySelector(".planetContainer"),
+      buttonList = document.querySelector(".buttonGroup"),
+      dropZones = document.querySelectorAll(".planetContainer"),
       container = document.querySelectorAll("#planetSection *");
 
 // Audio handling variables
 const theAudio = document.querySelector("audio[id]"),
       trackButtons = document.querySelectorAll(".track-ref");
 
+let instAudioCount = [];
+
     //Drag and Drop Functions
     // -----------------------------------------------------------------
     // 1 - Targetting element
     // The drag event, which will scrape the ID of any element that is trying to be dragged and store it in a disposable event variable
       function startDrag(event) {
-		console.log("started dragging");
+		// console.log("started dragging");
 		// save a reference to the element we're dragging
 		event.dataTransfer.setData("draggedElement", event.target.id);
 	}
@@ -38,7 +41,7 @@ const theAudio = document.querySelector("audio[id]"),
     //Prevent the default action from happening
     function draggedOver(event) {
 		event.preventDefault();
-		console.log("dragged over me");
+		// console.log("dragged over me");
 	}
 
     // 3 - placing the dragged element into the new element space
@@ -63,29 +66,50 @@ const theAudio = document.querySelector("audio[id]"),
 		console.log(`dropped this element:`, scrapedID);
 	}
 
-    // we need to save which instrument it is so we can loop it later
+
+    //Audio functions
     function instArray (scrapedID) {
         // make a new variable based off the passed-in ID and add "-audio" to it
         let currentAudio = scrapedID + "-audio";
         console.log(`the chosen instrument:`, currentAudio);
         // make a new variable that gets the audio from the HTML and adds our currentAudio variable to it to make a complete choice
-        let instAudio = [document.querySelector(`audio[id=${currentAudio}]`)];
-        
-        playInst
+        let instAudio = (document.querySelector(`audio[id=${currentAudio}]`));
+        // 
+        instAudioCount.push(currentAudio);
+        // instAudio.push(currentAudio);
+
+        console.log("currently playing audio", instAudio);
+        console.log("what is playing", instAudioCount);
+
+        playInst(instAudio);
     }
 
-    //Audio functions
-    function playInst(scrapedID) { 
-        
-        // this needs to loop
+   
+    function playInst(instAudio) { 
+
+        instAudio.play();
+        instAudio.loop = true;
+
         console.log(`current tracks playing: `, instAudio);
-        instAudio[0].play();
     }
 
     function reset() {
 
+        // put children back where they came from
+        console.log("the drop zones: ", dropZones);
+        console.log("the draggable buttons: ", draggableButtons);
+        // moves the image but not the audio
+		dropZones.forEach(zone => {
+			if(zone.childElementCount > 0) {
+                // let bastardAudio = zone.getElementbyID;
+                // debugger;
+                // bastardAudio.stop();
+                console.log("first element child: ", buttonList.firstElementChild);
+				buttonList.appendChild(zone.firstElementChild);
+			}
+		})
+        // stops the audio
     }
-
 
     function rewindInst() {
         theAudio.currentTime = 0;
